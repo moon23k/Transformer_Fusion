7e2f2ff4b16564202fba22b2a89acd58123e048b
moon <d.moon23k@gmail.com>
2024-01-07 01:29:52 +0900

    code updates

--- model/simple.py
+++ model/simple.py
@@ -40,59 +40,6 @@ class Decoder(nn.Module):
 
 
 
-class TorchModel(nn.Module):
-    def __init__(self, config):
-        super(TorchModel, self).__init__()
-        
-        self.device = config.device
-        self.pad_id = config.pad_id
-        self.vocab_size = config.vocab_size
-
-        self.encoder = AutoModel(config.mname)
-        self.decoder = Decoder(config)
-        self.generator = nn.Linear(config.hidden_dim, self.vocab_size)
-
-        self.criterion = nn.CrossEntropyLoss()
-        self.out = namedtuple('Out', 'logit loss')
-
-    
-    def pad_mask(self, x):
-        return x == self.pad_id
-
-
-    @staticmethod    
-    def shift_y(x):
-        return x[:, :-1], x[:, 1:]    
-
-
-    def dec_mask(self, x):
-        sz = x.size(1)
-        return torch.triu(torch.full((sz, sz), float('-inf')), diagonal=1).to(self.device)
-
-
-    def forward(self, input_ids, attention_mask, labels):
-        y, label = self.shift_y(y)
-
-        #Masking
-        e_mask = self.pad_mask(x)
-        d_mask = self.dec_mask(y)
-        
-        #Actual Processing
-        memory = self.encoder(input_ids=x, e_mask)
-        dec_out = self.decoder(y, memory, e_mask, d_mask)
-        logit = self.generator(dec_out)
-        
-        #Getting Outputs
-        self.out.logit = logit
-        self.out.loss = self.criterion(
-            logit.contiguous().view(-1, self.vocab_size), 
-            label.contiguous().view(-1)
-        )
-        
-        return self.out
-
-
-
 
 class SimpleModel(nn.Module):
     def __init__(self, config):
